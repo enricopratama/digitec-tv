327a32ba041d853608d06fac6a9c3ba8df708704
Enrico <enricopratama10@gmail.com>
2023-07-20 11:29:55 +0700

    Fixed navbar hover red circle; added BG blur in home page

--- public/javascript/cursorNormal.js
+++ public/javascript/cursorNormal.js
@@ -4,8 +4,6 @@ document.addEventListener('pointermove', (pos) => {
   let x = (pos.clientX / window.innerWidth) * 100;
   let y = ((pos.clientY + window.pageYOffset) / window.innerHeight) * 100;
 
-  y -= 0.044125 * window.innerWidth; // Changed to higher from 0.05
-
   if (window.innerWidth < 290) {
     y -= 30;
   } else if (window.innerWidth > 300 && window.innerWidth < 821) {
---
+++ public/javascript/navbarExpand.js
@@ -0,0 +1,33 @@
+// For scroll function:
+    function handleScroll() { 
+        const logo = document.querySelector(".logo-digitec"); // Declare 'logo' variable here
+        var scrollY = window.scrollY;
+        if (scrollY > 100 && window.innerWidth > 992) {
+            logo.style.width = "180px";
+        } else if (window.innerWidth < 450) {
+            logo.style.width = "60vw";
+        } else {
+            logo.style.width = "100%";
+        }
+    }
+    window.addEventListener("scroll", handleScroll);
+
+// For hover over function:
+
+    function setLogoFullWidth() {
+        const logo = document.querySelector(".logo-digitec");
+        logo.style.width = "100%";
+    }
+
+    function resetLogoWidth() {
+        var scrollY = window.scrollY;
+        if (scrollY === 0) {
+            logo.style.width = "100%";
+        } 
+        const logo = document.querySelector(".logo-digitec");
+        logo.style.width = "180px";
+    }
+
+    const navbar = document.getElementById("main-navbar");
+    navbar.addEventListener("mouseover", setLogoFullWidth);
+    navbar.addEventListener("mouseleave", resetLogoWidth);
