a7ddafee606aef0a2bd2f75c04d040ea67abb1c3
Enrico <enricopratama10@gmail.com>
2023-07-26 16:44:40 +0700

    Revised minor cursor spotlight

--- public/javascript/cursorHome.js
+++ public/javascript/cursorHome.js
@@ -4,8 +4,7 @@ function updateMaskPosition(pos) {
   let x = (pos.clientX / window.innerWidth) * 100;
   let y = ((pos.clientY + window.pageYOffset) / window.innerHeight) * 100;
 
-  // y -= window.innerWidth * 0.0575; // Optimization for home screen
-  // y -= 80;
+  y -= window.innerWidth * 0.0575; // Optimization for home screen
 
   mask.style.setProperty('--mouse-x', x + '%');
   mask.style.setProperty('--mouse-y', y + '%');
