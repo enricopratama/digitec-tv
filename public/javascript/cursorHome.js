c8819ac60c1ef71643a12dfa1dd85a5b4de1c162
Enrico <enricopratama10@gmail.com>
2023-07-28 10:38:52 +0700

    Revision cursor v2

--- public/javascript/cursorHome.js
+++ public/javascript/cursorHome.js
@@ -1,41 +1,33 @@
-// const mask = document.querySelector('.mask');
-
-// function updateMaskPosition(pos) {
-//   let x = (pos.clientX / window.innerWidth) * 100;
-//   let y = ((pos.clientY + window.pageYOffset) / window.innerHeight) * 100;
-
-//   y -= 85; 
-
-//   if(window.innerWidth >= 1450) {
-//     y += window.innerWidth * 0.03;
-//   } 
-
-//   mask.style.setProperty('--mouse-x', x + '%');
-//   mask.style.setProperty('--mouse-y', y + '%');
-// }
-
-// document.addEventListener('pointermove', (pos) => {
-//   updateMaskPosition(pos);
-// });
-
-// window.addEventListener('resize', () => {
-
-//   const currentPos = { clientX: event.clientX, clientY: event.clientY };
-//   updateMaskPosition(currentPos);
-// });
-
 const mask = document.querySelector('.mask');
 
 function updateMaskPosition(pos) {
   let x = (pos.clientX / window.innerWidth) * 100;
   let y = ((pos.clientY + window.pageYOffset) / window.innerHeight) * 100;
 
-  y -= 86; // Optimization for carousel on top (home screen)
-  if (window.innerWidth < 992 && window.innerWidth > 609) {
-    y += window.innerWidth * 0.0242;
-  } else if (window.innerWidth < 608) {
-    y += window.innerWidth * 0.0742;
-  }
+  // y -= 86; // Optimization for carousel on top (home screen)
+  // if (window.innerWidth < 992 && window.innerWidth > 609) {
+  //   y += window.innerWidth * 0.0242;
+  // } else if (window.innerWidth < 608) {
+  //   y += window.innerWidth * 0.0742;
+  // }
+
+  if (window.innerWidth >= 1280) {
+    y -= 78;
+  } else if (window.innerWidth < 1280 && window.innerWidth > 1020) {
+    y -= 80;
+  } else if (window.innerWidth < 1020 && window.innerWidth > 920) {
+    y -= 75;
+  } else if (window.innerWidth < 920 && window.innerWidth > 820) {
+    y -= 65;
+  } else if (window.innerWidth < 820 && window.innerWidth > 720) {
+    y -= 55;
+  } else if (window.innerWidth < 720 && window.innerWidth > 620) {
+    y -= 45;
+  } else if (window.innerWidth < 620 && window.innerWidth > 520) {
+    y -= 35;
+  } else if (window.innerWidth < 520 && window.innerWidth > 220) {
+    y -= 25;
+  } 
 
   mask.style.setProperty('--mouse-x', x + '%');
   mask.style.setProperty('--mouse-y', y + '%');
@@ -46,7 +38,7 @@ document.addEventListener('pointermove', (pos) => {
 });
 
 window.addEventListener('resize', () => {
-  // Recalculate mask position when the window is resized
+
   const currentPos = { clientX: event.clientX, clientY: event.clientY };
   updateMaskPosition(currentPos);
 });
