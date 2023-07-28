cc341fc58df1a75e48623e1d6695eccd93fa1462
Enrico <enricopratama10@gmail.com>
2023-07-28 09:24:52 +0700

    Cursor home minor bug fix (testing)

--- public/javascript/cursorHome.js
+++ public/javascript/cursorHome.js
@@ -1,14 +1,41 @@
+// const mask = document.querySelector('.mask');
+
+// function updateMaskPosition(pos) {
+//   let x = (pos.clientX / window.innerWidth) * 100;
+//   let y = ((pos.clientY + window.pageYOffset) / window.innerHeight) * 100;
+
+//   y -= 85; 
+
+//   if(window.innerWidth >= 1450) {
+//     y += window.innerWidth * 0.03;
+//   } 
+
+//   mask.style.setProperty('--mouse-x', x + '%');
+//   mask.style.setProperty('--mouse-y', y + '%');
+// }
+
+// document.addEventListener('pointermove', (pos) => {
+//   updateMaskPosition(pos);
+// });
+
+// window.addEventListener('resize', () => {
+
+//   const currentPos = { clientX: event.clientX, clientY: event.clientY };
+//   updateMaskPosition(currentPos);
+// });
+
 const mask = document.querySelector('.mask');
 
 function updateMaskPosition(pos) {
   let x = (pos.clientX / window.innerWidth) * 100;
   let y = ((pos.clientY + window.pageYOffset) / window.innerHeight) * 100;
 
-  y -= window.innerWidth * 0.0575; // Optimization for home screen
-
-  if(window.innerWidth >= 1450) {
-    y += window.innerWidth * 0.03;
-  } 
+  y -= 86; // Optimization for carousel on top (home screen)
+  if (window.innerWidth < 992 && window.innerWidth > 609) {
+    y += window.innerWidth * 0.0242;
+  } else if (window.innerWidth < 608) {
+    y += window.innerWidth * 0.0742;
+  }
 
   mask.style.setProperty('--mouse-x', x + '%');
   mask.style.setProperty('--mouse-y', y + '%');
@@ -19,7 +46,7 @@ document.addEventListener('pointermove', (pos) => {
 });
 
 window.addEventListener('resize', () => {
-
+  // Recalculate mask position when the window is resized
   const currentPos = { clientX: event.clientX, clientY: event.clientY };
   updateMaskPosition(currentPos);
 });
--- public/javascript/cursorNormal.js
+++ public/javascript/cursorNormal.js
@@ -4,11 +4,6 @@ function updateMaskPosition(pos) {
   let x = (pos.clientX / window.innerWidth) * 100;
   let y = ((pos.clientY + window.pageYOffset) / window.innerHeight) * 100;
 
-  // x kurang kanan dikit
-  // x += 1;
-
-
-
   mask.style.setProperty('--mouse-x', x + '%');
   mask.style.setProperty('--mouse-y', y + '%');
 }
